a65281008f2c36ad53e7d04ec824eb6699befb16
Lauren Melendez <lmelende@stevens.edu>
2022-02-16 17:41:59 -0500

    alphabet spacing fix

--- components/ByName.js
+++ components/ByName.js
@@ -9,12 +9,12 @@ function ByName({Letter, setLetter}) {
     
     return (
         <div className="flex md:flex-row flex-col w-[100%]">
-            <div className='flex justify-evenly w-[100%] flex-wrap '>
+            <div className='flex justify-around w-[100%] flex-wrap '>
             {
                 [...options1].map(option => <button key={option} className={`font-Lora lg:text-[25px] text-[22px] text-medGreen ${option===Letter ? 'font-bold underline' : 'font-normal no-underline'}`} onClick={() => setLetter(option)}>{option}</button> )
             }         
             </div> 
-            <div className='flex justify-evenly w-[100%] flex-wrap '>
+            <div className='flex justify-around w-[100%] flex-wrap '>
             {
                 [...options2].map(option => <button key={option} className={`font-Lora lg:text-[25px] text-[22px] text-medGreen ${option===Letter ? 'font-bold underline' : 'font-normal no-underline'}`} onClick={() => setLetter(option)}>{option}</button> )
             }         
