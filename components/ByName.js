ee0a3cce2852e9833fc29e660d6afb536fcc644c
Lauren Melendez <lmelende@stevens.edu>
2022-02-16 16:59:49 -0500

    ByName change

--- components/ByName.js
+++ components/ByName.js
@@ -4,15 +4,23 @@ import DropDown from './Dropdown'
 
 function ByName({Letter, setLetter}) {
     const router = useRouter();
-    const options = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const options1 = "ABCDEFGHIJKLM"
+    const options2 = "NOPQRSTUVWXYZ"
     
     return (
-            
-        <div className='flex justify-evenly w-[100%] flex-wrap'>
-        {
-            [...options].map(option => <button key={option} className={`font-Lora lg:text-[25px] text-[20px] text-medGreen ${option===Letter ? 'font-bold underline' : 'font-normal no-underline'}`} onClick={() => setLetter(option)}>{option}</button> )
-        }         
-        </div>    
+        <div className="flex md:flex-row flex-col w-[100%]">
+            <div className='flex justify-evenly w-[100%] flex-wrap '>
+            {
+                [...options1].map(option => <button key={option} className={`font-Lora lg:text-[25px] text-[22px] text-medGreen ${option===Letter ? 'font-bold underline' : 'font-normal no-underline'}`} onClick={() => setLetter(option)}>{option}</button> )
+            }         
+            </div> 
+            <div className='flex justify-evenly w-[100%] flex-wrap '>
+            {
+                [...options2].map(option => <button key={option} className={`font-Lora lg:text-[25px] text-[22px] text-medGreen ${option===Letter ? 'font-bold underline' : 'font-normal no-underline'}`} onClick={() => setLetter(option)}>{option}</button> )
+            }         
+            </div> 
+        </div>
+           
         
         
     )
