e63903758774b247a8778f044add44141c37acca
Lauren Melendez <lmelende@stevens.edu>
2022-02-22 18:57:10 -0500

    mobile css fix

--- components/Filter.js
+++ components/Filter.js
@@ -9,7 +9,7 @@ function Filter({State, setState, Letter, setLetter, Country, setCountry}) {
             <div className='flex flex-col md:items-start items-center'>
                 <div className='pb-[30px]'>
                     <h1 className='font-Lora lg:text-[30px] md:text-[25px] text-[20px] text-lightGreen'>Filter By:</h1>
-                    <select className="font-Nunito lg:text-[20px] text-[15px] text-medGreen form-select block mt-1 bg-[#FFFFFF]" value={State} onChange={(e) => setState(e.target.value)}>
+                    <select className="filter-select font-Nunito lg:text-[20px] text-[15px] text-medGreen form-select block mt-1 bg-[#FFFFFF]" value={State} onChange={(e) => setState(e.target.value)}>
                         <option selected value='ByName'>Name</option>
                         <option value='ByCountry'>Country</option>    
                     </select>
